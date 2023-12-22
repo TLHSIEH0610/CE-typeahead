b75b1afc542b18a63838a919eb07796f4432f086
Arnie Hsieh <tlhsieh0610@gmail.com>
2023-12-22 18:59:20 +0800

    add express /states api

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
-// const express = require("express");
-// const cors = require("cors");
+// requirement:
+// 1.show at most 8 results
+// 2.results should be ordered alphabetically
+
 import express from "express";
 import cors from "cors";
+import states from "./states.js";
 
 const server_port = process.env.server_port || 4000;
+const maxResult = 8;
 
 const app = express();
 app.use(cors());
+
+app.get("/states", (req, res) => {
+  const query = req.query.search.toLowerCase();
+  const result = states
+    .filter((state) => state.toLowerCase().includes(query))
+    .sort()
+    .slice(0, maxResult);
+  res.status(200).json(result);
+});
 
 app.listen(server_port, () => {
   console.log(`listening to port ${server_port}`);
