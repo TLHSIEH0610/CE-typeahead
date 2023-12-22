ea1207bf58bf1f0ca4cd8a0152f9fa45a657ec10
Arnie Hsieh <tlhsieh0610@gmail.com>
2023-12-22 18:15:28 +0800

    server: use ES module

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+// const express = require("express");
+// const cors = require("cors");
+import express from "express";
+import cors from "cors";
 
 const server_port = process.env.server_port || 4000;
 
